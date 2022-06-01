1f4548d5a0312e6fff8cc9040d36c9a10c1d531e
Ara Kanic <ji.arakani@gmail.com>
2022-06-01 10:08:52 -0400

    Update milliseconds.js

--- milliseconds.js
+++ milliseconds.js
@@ -1,3 +1,9 @@
 // This function accepts h hours, m minutes and s seconds to return total time in milliseconds.
 
 const past = (h, m, s) => 1000 * (s + (60 * (m + (60 * h))))
+
+past(0,1,1) // 61000
+past(1,1,1) // 3661000
+past(0,0,0) // 0
+past(1,0,1) // 3601000
+past(1,0,0) // 3600000
